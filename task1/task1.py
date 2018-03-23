97a20eda5046f45334e9cc762dff13bbf0e44668
skcse <saurabhk44@gmail.com>
2018-03-23 19:20:40 +0530

    Modified task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,34 +1,37 @@
 import configparser
 import os
+import json
+import requests
 
-def config_file():
+def config_file(org):
 	cfile= configparser.ConfigParser()
 	cfile['general']={
-						'short_name':'GrimoireLab',
+						'short_name':'Girmoirelab',
 						'update' :'false',
 						'sleep':' 0',
 						'min_update_delay ':' 10',
 						'debug':' true',
-						'logs_dir':'/tmp/logs',
-						'kibana':'"5"'
+						'logs_dir':'/tmp/',
+						'kibana':'"6"'
 					}
-	cfile['projects']={'projects_file':'projects.json'}
+	cfile['projects']={'projects_file':get_repo(org)}
 	cfile['es_collection']={
-							'url':'http://localhost:9200',
+							'url':'http://127.0.0.1:9200',
 							'user':'',
 							'password':''
 							}
 	cfile['es_enrichment']={
-							'url':'http://localhost:9200',
+							'url':'http://127.0.0.1:9200',
 							'user':' ',
 							'password':' ',
-							'autorefresh': 'false'
+							'autorefresh': 'false',
+							
 							}
 	cfile['sortinghat']={
 						'host' : 'localhost',
-						'user' : 'saurabhu',
-						'password' : 'saurabhp',
-						'database' : 'shdb',
+						'user' : 'XXXX',
+						'password' : 'XXXX',
+						'database' : 'XXXX',
 						'load_orgs' : 'false',
 						'unify_method':'',
 						'unaffiliated_group' : 'Unknown',
@@ -39,7 +42,7 @@ def config_file():
 						'bots_names' : '[Beloved Bot]'
 						}
 	cfile['panels']={
-					'kibiter_time_from': '"now-90d"',
+					'kibiter_time_from': '"now-180d"',
 					'kibiter_default_index':'"git"'	
 					}
 	cfile['phases']={
@@ -52,12 +55,43 @@ def config_file():
 				'raw_index' : 'git_test-raw',
 				'enriched_index' : 'git_test'
 				}
-	with open('config.ini','w') as configfile:
+	cfile['github']={
+					'raw_index' : 'github_test-raw',
+					'enriched_index' : 'github_test',
+					'api-token' : 'XXXX',
+					'sleep-for-rate' : 'true'
+					}
+	with open('config_'+org+'.cfg','w') as configfile:
 		cfile.write(configfile)
 
 
+def get_repo(org):
+	org_url='https://api.github.com/orgs/'+org+'/repos'
+	data={}
+	data[org]={}
+	data[org]['git']=[]
+	data[org]['github']=[]
+	r = requests.get(org_url)
+	org_file=r.json()
+	# print(org_file)
+	for repo in org_file:
+		# print(repo)
+		# print(" ")
+		if repo['fork']==False:
+			repo_url=repo['html_url']
+			data[org]['git'].append(
+				repo_url
+				)
+			data[org]['github'].append(
+				repo_url
+				)
+
+	with open('projects_'+org+'.json','w') as outfile:
+		json.dump(data,outfile,default=str)
+		return 'projects_'+org+'.json'
 
 
 
 if __name__ == '__main__':
-	config_file()
+	org=input("Enter organisation name\n")
+	config_file(org)
